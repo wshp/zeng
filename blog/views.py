ee932b59e39d06429a35a80d7c7cf3708613717b
Administrator <935535414@qq.com>
2019-03-22 19:28:24 +0800

    0.1版本 最初始 开始增删改查

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    po=Post.objects.all()
+    return render(request,"index.html",{'po':po})
